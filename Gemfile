da706c374f23ded185f7865055e4bf2cabc7e81a
aaaa777 <aaaa777@users.noreply.github.com>
2024-10-03 02:34:59 +0900

    add: uuidv7 as id

--- Gemfile
+++ Gemfile
@@ -45,6 +45,7 @@ gem "bootsnap", require: false
 # gem "image_processing", "~> 1.2"
 
 gem 'devise'
+gem 'uuid7'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- db/migrate/20241002161457_create_members.rb
+++ db/migrate/20241002161457_create_members.rb
@@ -2,7 +2,7 @@ class CreateMembers < ActiveRecord::Migration[7.1]
   def change
     create_table :members do |t|
       t.string :status
-      t.references :circle, null: false, foreign_key: true
+      t.references :circle, type: :uuid, null: false, foreign_key: true
 
       t.timestamps
     end
