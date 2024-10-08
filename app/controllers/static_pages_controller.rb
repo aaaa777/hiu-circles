41c6d787016258fd036d8f67399c900437beebf5
aaaa777 <aaaa777@users.noreply.github.com>
2024-10-09 07:32:00 +0900

    fix: some

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -13,6 +13,6 @@ class StaticPagesController < ApplicationController
   end
 
   def redirect_to_root
-    redirect_to home_url(allow_other_host: true)
+    redirect_to home_url, allow_other_host: true
   end
 end
